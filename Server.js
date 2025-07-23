baf58c28c4f26c50d4f07dae84048482d6ae3b03
Reema_abde <Reemabdella702@gmail.com>
2025-07-23 12:11:53 +0300

    Initialize Node

---
+++ Server.js
@@ -0,0 +1,9 @@
+const os = require('os');
+const path = require('path');
+const Math = require('./Math');
+
+console.log('Hello World');
+
+
+console.log(Math.divide(10, 5)); 
+console.log(os.platform())
